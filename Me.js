fbd795b1498b60821d334719256321f01b3291a8
buddy102306 <Buddysenoloslll@gmail.com>
2026-09-21 10:30:15 +0800

    Update my first commit

--- Me.js
+++ Me.js
@@ -1,3 +1,14 @@
+console.log("===== BIODATA =====");
+
+console.log("Name: Buddy");
+console.log("Age: 21");
+console.log("Gender: Male");
+console.log("Address: Calbayog City, Samar");
+console.log("Course: Bachelor of Science in Information Technology");
+console.log("School: Northwest Samar State University");
+
+console.log("===== ABOUT ME =====");
 console.log("Hello, World!");
 console.log("My name is Buddy.");
 console.log("I am learning JavaScript.");
+console.log("I enjoy learning new things.");
